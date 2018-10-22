9fc97f9b25b8adbd816002791b3292af8c2671bf
vincent LES <vincent.leschaeve@gmail.com>
2018-10-22 21:50:45 +0200

    Part3

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vince
+ * Date: 22/10/18
+ * Time: 19:37
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route ("/")
+     * @return Response
+     */
+
+    public function index(){
+
+        return $this->render('home.html.twig');
+
+    }
+}
---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vince
+ * Date: 22/10/18
+ * Time: 18:02
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number")
+     * @return Response
+     * @throws \Exception
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
